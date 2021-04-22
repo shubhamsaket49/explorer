ea76766389c2909e62b99ed42ef78f0ccae1275a
Shubham Saket <63151463+shubhamsaket49@users.noreply.github.com>
2021-04-22 12:46:05 +0530

    Add files via upload

---
+++ MenuScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    
+    public void Quit()
+    {
+     Debug.Log("Quit pressed!");  
+     //Application.Quit();  //works when app is build not in editor
+     UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void Options()
+    {
+     Debug.Log("Options pressed!");  
+     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+    public void Play()
+    {
+     Debug.Log("Play pressed!");  
+     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +2);
+    }
+   
+    
+}
